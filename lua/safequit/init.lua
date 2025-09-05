787d61d6d28d1036745ef56d0b7c8aaa67aee4ee
Brandon Williams <brandonmakaiw@gmail.com>
2025-09-04 22:52:39 -0400

    perf: Remove unnecessary buffer loop

--- lua/safequit/init.lua
+++ lua/safequit/init.lua
@@ -3,20 +3,16 @@ local M = {}
 function M.check_dirty_buffers()
   local files = vim.fn.getbufinfo({ buflisted = 1 })
   local dirty_buffers = {}
+
   for _, buf in ipairs(files) do
       local buftype = vim.api.nvim_get_option_value("buftype", {buf = buf.bufnr})
       if buf.changed and buftype == "" then
-          table.insert(dirty_buffers, buf)
+          table.insert(dirty_buffers, buf.name)
       end
   end
 
   if #dirty_buffers > 1 then
-      local filenames = {}
-      for _, buf in ipairs(dirty_buffers) do
-        table.insert(filenames, buf.name)
-      end
-      filenames = table.concat(filenames, ", ")
-
+      local filenames = table.concat(dirty_buffers, ", ")
       vim.notify("Dirty buffers: " .. filenames)
       vim.cmd("wqa")
   end
